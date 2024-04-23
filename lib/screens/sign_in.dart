39fa5bed0de8b4fca34132d4028edfd4ae5b6d33
boterop <boterop22@gmail.com>
2024-04-23 00:32:05 -0500

    Sign In template

--- lib/screens/sign_in.dart
+++ lib/screens/sign_in.dart
@@ -9,28 +9,64 @@ class SignIn extends StatefulWidget {
 }
 
 class _SignInState extends State<SignIn> {
-  late String test = "";
+  String email = "";
+  String password = "";
 
   @override
   void initState() {
     super.initState();
-    APIService().health().then((response) => {
-          setState(() {
-            test = response;
-          })
-        });
+    APIService()
+        .health()
+        .then((response) => {if (!response) Navigator.pop(context)})
+        .catchError((error) => {Navigator.pop(context)});
   }
 
   @override
   Widget build(BuildContext context) {
+    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
     return Scaffold(
-      appBar: AppBar(),
-      body: Center(
-        child: Hero(
-          tag: 'addHabit',
-          child: Text(test, style: const TextStyle(fontSize: 24)),
-        ),
-      ),
-    );
+        appBar: AppBar(),
+        body: Center(
+            child: Form(
+          key: formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email:',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password:',
+                ),
+                validator: (String? value) {
+                  if (value == null || value.isEmpty) {
+                    return 'Please enter some text';
+                  }
+                  return null;
+                },
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 16.0),
+                child: ElevatedButton(
+                  onPressed: () {
+                    if (formKey.currentState!.validate()) {
+                      // Process data.
+                    }
+                  },
+                  child: const Text('Sign In'),
+                ),
+              ),
+            ],
+          ),
+        )));
   }
 }
