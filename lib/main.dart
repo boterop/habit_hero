b175ffb75686fa3d0eefd2203c70623b6c98c2a3
Santiago Botero <98826652+boterop@users.noreply.github.com>
2024-04-23 01:24:18 -0500

    Create en and es translations (#5)

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:habit_hero/l10n/l10n.dart';
 import 'package:habit_hero/screens/sign_in.dart';
 import 'package:habit_hero/themes/dark.dart';
 import 'package:habit_hero/themes/light.dart';
 import 'package:habit_hero/screens/home.dart';
 import 'package:habit_hero/widgets/toggle_theme.dart';
 import 'package:habit_hero/widgets/user_button.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() async {
   await dotenv.load();
@@ -34,6 +37,13 @@ class MainState extends State<MainApp> {
       themeMode: themeMode,
       theme: Light.theme,
       darkTheme: Dark.theme,
+      localizationsDelegates: const [
+        AppLocalizations.delegate,
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      supportedLocales: L10n.locales,
       home: Scaffold(
           appBar: AppBar(
             leading: const UserButton(route: SignIn()),
